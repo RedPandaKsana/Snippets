744bc49ddad4f56feba3f19cb7e689d119c9dc3b
RedPandaKsana <ksanafeniks@yandex.ru>
2020-11-30 16:27:35 +0300

    Неудачная попытка вывода данных из БД

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from .models import Snippet
 
 
 def get_base_context(request, pagename):
@@ -21,3 +22,7 @@ def add_snippet_page(request):
 def snippets_page(request):
     context = get_base_context(request, 'Просмотр сниппетов')
     return render(request, 'pages/view_snippets.html', context)
+
+def items(request):
+    items = Snippet.objects.all()
+    return render(request, "pages/view_snippets.html", {"items": items})
